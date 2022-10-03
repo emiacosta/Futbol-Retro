0226af2de020cfe8d3360ef4892ebe8021c27255
Emiliano Acosta <emiacosta97@gmail.com>
2022-10-03 20:58:39 -0300

    Cambios en NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,21 +1,18 @@
 import { Navbar, Container } from "react-bootstrap";
 import CartWidget from '../CartWidget/CartWidget.js';
+import SeccionesNavBar from "../SeccionesNavBar/SeccionesNavBar.js";
 import './NavBar.css';
 
 const NavBar = () => {
   return (
     <>
-    <Navbar expand="lg">
-      <Container>
-        <h1 href="#" className="logo">Fútbol Retro</h1>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        <h5 href="camisetas" className="seccionesNav">Camisetas</h5>
-        <h5 href="shorts" className="seccionesNav">Shorts</h5>
-        <h5 href="buzos" className="seccionesNav">Buzos</h5>
-        <h5 href="botines" className="seccionesNav">Botines</h5>
-        <CartWidget/>
-      </Container>
-    </Navbar>
+      <Navbar expand="lg">
+        <Container>
+          <h1 href="#" className="logo">Fútbol Retro</h1>
+          <SeccionesNavBar/>
+          <CartWidget />
+        </Container>
+      </Navbar>
     </>
   );
 }
